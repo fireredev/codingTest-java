a0edbdbef2f69c0aad6300b294364061da0f5e9b
fireredev <148295523+fireredev@users.noreply.github.com>
2024-03-12 23:07:09 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.18 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String [] arr =s.split(" ");
+        for(int i =0;i<arr.length;i++){
+            try{answer += Integer.parseInt(arr[i]);}
+            catch(NumberFormatException e){
+                answer -= Integer.parseInt(arr[i-1]);
+            }
+                
+        }
+        return answer;
+    }
+}
