97999022427730777561905fe5cb3952deea75eb
fireredev <148295523+fireredev@users.noreply.github.com>
2024-03-13 22:31:48 +0800

    [level 0] Title: 잘라서 배열로 저장하기, Time: 1.26 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_str, int n) {
+        String[] answer = {};
+        String str = my_str.substring(0,n);
+        List<String>li = new ArrayList<>();
+         while(my_str.length()>=n){
+             li.add(my_str.substring(0,n));
+             my_str = my_str.substring(n);
+         }
+        if(!my_str.isEmpty())li.add(my_str);
+        return li.stream().toArray(String[]::new);
+    }
+}
