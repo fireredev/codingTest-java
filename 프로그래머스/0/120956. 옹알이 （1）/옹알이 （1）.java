733083e8fa52e48f4fb5815b63bd261597ba35d8
fireredev <148295523+fireredev@users.noreply.github.com>
2024-03-15 15:51:45 +0800

    [level 0] Title: 옹알이 (1), Time: 0.12 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,14 +3,14 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for(int i=0;i<babbling.length;i++){
-            babbling[i]= babbling[i].replace("aya"," ")
+            babbling[i]= babbling[i]
+                       .replace("aya"," ")
                        .replace("ye"," ")
-                    .replace("woo"," ")
-                .replace("ma"," ");
-        }
-        for(int i=0;i<babbling.length;i++){
+                       .replace("woo"," ")
+                       .replace("ma"," ");
             if(babbling[i].isBlank()) answer++;
         }
+        
         return answer;
     }
 }
