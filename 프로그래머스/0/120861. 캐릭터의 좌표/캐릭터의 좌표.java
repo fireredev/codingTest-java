c7167f350227f4850f54b2e1eb94c36e6903e6ca
fireredev <148295523+fireredev@users.noreply.github.com>
2024-03-15 15:04:56 +0800

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -6,9 +6,9 @@ class Solution {
         int y =0;
         for(int i =0;i<keyinput.length;i++){
             if(keyinput[i].equals("up") && board[1]/2 >= Math.abs(y+1)) y++;
-            else if(keyinput[i].equals("down") && board[1]/2 >= Math.abs(y-1)) y--;
-            else if(keyinput[i].equals("right") && board[0]/2 >= Math.abs(x+1)) x++;
-            else if(keyinput[i].equals("left") && board[0]/2 >= Math.abs(x-1)) x--;
+             if(keyinput[i].equals("down") && board[1]/2 >= Math.abs(y-1)) y--;
+             if(keyinput[i].equals("right") && board[0]/2 >= Math.abs(x+1)) x++;
+            if(keyinput[i].equals("left") && board[0]/2 >= Math.abs(x-1)) x--;
         }
         if(board[0]/2<=Math.abs(x)) {
             if(Math.abs(x)==x) {
