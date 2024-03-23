8bf1b6de98c0c9e4dc2985cc4ac5a1e8580d5a7d
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-03-24 00:28:46 +0800

    [level 1] Title: 최대공약수와 최소공배수, Time: 4.18 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -4,10 +4,7 @@ import java.util.stream.*;
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int [2];
-        int s=Math.min(n,m);
-        int r=Math.max(n,m);
-        
-        answer [0] = sol(s,r);
+        answer [0] = sol(n,m);
         answer [1] = n * m / answer[0];
         return answer;
     }
@@ -15,7 +12,6 @@ class Solution {
         int answer =0;
         int [] arrN = IntStream.rangeClosed(1,n).filter(f -> n % f == 0).toArray();
         int [] arrM = IntStream.rangeClosed(1,m).filter(f -> m % f == 0).toArray();
-        
         for(int i=0;i<arrN.length;i++){
             for(int j=0;j<arrM.length;j++){
                 if(arrN[i]==arrM[j] && arrN[i] > answer){
