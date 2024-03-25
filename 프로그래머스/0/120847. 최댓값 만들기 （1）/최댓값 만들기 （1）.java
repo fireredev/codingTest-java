d9e9ab3d8bddff8481b70f422ae6a96b081d766a
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-03-25 22:45:04 +0800

    [level 0] Title: 최댓값 만들기 (1), Time: 2.18 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+       
+        numbers = Arrays.stream(numbers).sorted().toArray();
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
+    }
+}
