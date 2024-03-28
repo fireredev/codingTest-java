1665f2a23354ffca173fa3395363e13fd9e23c0b
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-03-28 21:22:21 +0800

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.19 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        answer = (int)Math.ceil(n/(double)slice);
+        
+        
+        
+        return answer; 
+        
+    }
+}
