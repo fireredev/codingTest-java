87a0d12b4ab44228a7b13d056f5e988ec3bc3029
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-03-30 22:54:44 +0800

    [level 0] Title: 저주의 숫자 3, Time: 3.24 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] answer1 = IntStream.iterate(0, i -> i + 1) 
+               int[] answer1 = IntStream.iterate(0, i -> i + 1) 
                 .filter(i -> i % 3 != 0 && !Integer.toString(i).contains("3")) 
                 .limit(n) 
                 .toArray();
