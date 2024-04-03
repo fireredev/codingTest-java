c879ecc5ae79c0d68d4190d74c7c12b8452a1d7b
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-03 14:23:44 +0800

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        List<int[]>li = new ArrayList<>();
+        int sum = brown+yellow;
+        for(int i=3;i<=sum;i++){
+            if(sum % i ==0 && i>= sum / i && sum / i >= 3) 
+                li.add(new int[]{i,sum / i}); 
+        }
+        if(li.size()==1) return li.get(0);
+        
+        for(int i=0;i<li.size();i++){
+            if((li.get(i)[0] - 2) * (li.get(i)[1] - 2) == yellow) return li.get(i);
+        }
+        return answer;
+    }
+}
