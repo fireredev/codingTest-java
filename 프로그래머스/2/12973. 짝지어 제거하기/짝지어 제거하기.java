991ffee129ab41b247c6938991a079a1a8072781
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-03 13:16:30 +0800

    [level 2] Title: 짝지어 제거하기, Time: 41.90 ms, Memory: 60.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {   
+        
+        ArrayDeque<Character> aq = new ArrayDeque<>(); 
+        
+        for(int i=0;i<s.length();i++){
+            if(!aq.isEmpty() && aq.peek()==s.charAt(i)){
+                aq.pop();
+            }else{
+                aq.push(s.charAt(i));
+            }
+        }
+        
+        
+
+        return aq.isEmpty() ? 1 : 0;
+    }
+}
