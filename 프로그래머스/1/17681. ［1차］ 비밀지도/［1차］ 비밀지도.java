88df6add6edd24bac0ec82c2f4b543cce260dac2
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-04 19:57:30 +0800

    [level 1] Title: [1차] 비밀지도, Time: 1.32 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,16 @@
-import java.util.*;
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String [n] ;
-        String[] fMap = Arrays.stream(arr1).mapToObj(Integer::toBinaryString).toArray(String[]::new);
-        String[] sMap = Arrays.stream(arr2).mapToObj(Integer::toBinaryString).toArray(String[]::new);
-        
-        for(int i =0;i<fMap.length;i++){
-            answer[i]=String.valueOf(Long.parseLong(fMap[i])+Long.parseLong(sMap[i]));
-            String zero="0";
-            if(answer[i].length()!=n) answer[i]=zero.repeat(n-answer[i].length())+answer[i];
-            answer[i]=answer[i].replaceAll("[1-9]","#").replaceAll("0"," ");
-        }
-        
-        return answer;
-    }
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+      String[] answer = new String[n];
+      String temp;
+
+      for(int i = 0 ; i < n ; i++){
+          temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+          temp = temp.substring(temp.length() - n);
+          temp = temp.replaceAll("1", "#");
+          temp = temp.replaceAll("0", " ");
+          answer[i] = temp;
+      }
+
+      return answer;
+  }
 }
