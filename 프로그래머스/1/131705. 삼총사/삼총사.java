a40edec35a8c3349274cf3078cd8d2bb217c2c93
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-07 20:00:12 +0800

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<number.length;i++){
             for(int j=0;j<i;j++){
                 for(int k=0;k<j;k++){
-                    if(number[i]+number[j]+number[k]==0) answer++;
+                     if(number[i]+number[j]+number[k]==0) answer++;
                 }
             }
         }
