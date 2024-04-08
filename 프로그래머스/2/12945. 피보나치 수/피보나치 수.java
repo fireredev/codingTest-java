cf5deb18e1c529ac4a23975b264f2c0a49829cbd
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-08 22:59:48 +0800

    [level 2] Title: 피보나치 수, Time: 1.97 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int a=0,b=1,c;
-        
-        for(int i=2;i<=n;i++){
-            c = (a+b) % 1234567;
-            a = b;
-            b = c;
-        }
-                
-        return b;
+    int a = 0, b = 1, c;
+    if (n == 0) return a;
+    for (int i = 2; i <= n; i++) {
+        c = (a + b) % 1234567; // 여기에서 모듈로 연산을 적용
+        a = b;
+        b = c;
     }
+    return b;
+}
+
 }
