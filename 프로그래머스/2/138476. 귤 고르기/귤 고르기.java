f23b763d59f1f5a81904e42346a620e09a93ba43
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-24 16:14:53 +0800

    [level 2] Title: 귤 고르기, Time: 78.43 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,31 +3,26 @@ import java.util.stream.*;
 import java.util.function.Function;
 class Solution {
     public int solution(int k, int[] tangerine) {
+        List<Integer> li = Arrays.stream(tangerine)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(),
+                                               Collectors.counting()))
+                .values()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .map(Long::intValue)
+                .collect(Collectors.toList());
+        
         int answer = 1;
         
-       
-        List<Integer>li = getList(Arrays.stream(tangerine).
-                            boxed().collect(Collectors.toList()));
-        
-        
-        Collections.sort(li,Collections.reverseOrder());
         for(int i=0;i<li.size();i++){
             k=k-li.get(i);
             if(k<=0) return answer;
             answer++;
         }
+        
   
         return k;
     }
-    private List<Integer>getList(List<Integer>li){
-         List<Integer> occurrenceList = li.stream()
-                .collect(Collectors.groupingBy(Function.identity(),
-                        Collectors.counting())) // 각 원소의 등장 횟수를 계산
-                .values() // 등장 횟수 값들의 컬렉션
-                .stream()
-                .map(Long::intValue) // Long을 Integer로 변환
-                .collect(Collectors.toList()); // 결과를 리스트로 수집
-
-        return occurrenceList;
-    }
+   
 }
