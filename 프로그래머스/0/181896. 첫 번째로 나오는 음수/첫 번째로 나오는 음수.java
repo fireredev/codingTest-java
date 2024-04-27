806bfe3cfc20bc377672eda851c01f42ce97e41d
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-27 22:51:21 +0800

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -2,12 +2,9 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = -11;
         for(int i=0;i<num_list.length;i++){
-            if(num_list[i]<0) {
-                answer = i;
-                break;
-            }
+            if(num_list[i]<0) return answer = i;
         }
-        
-        return answer==-11? -1: answer ;
+        answer=-1;
+        return answer;
     }
 }
