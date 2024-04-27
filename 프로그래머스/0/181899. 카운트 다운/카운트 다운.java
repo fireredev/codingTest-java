5a8b7e3615c8c97195401cfee2f7a9359a7d5931
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-04-27 23:00:46 +0800

    [level 0] Title: 카운트 다운, Time: 2.30 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -2,13 +2,9 @@ import java.util.stream.*;
 import java.util.*;
 class Solution {
     public int[] solution(int start, int end_num) {
-        int[] answer = IntStream.rangeClosed(end_num,start).toArray();
+        int[] answer = IntStream.rangeClosed(-start,-end_num).map(i->-i).toArray();
         
-        for(int i = 0; i < answer.length / 2; i++) {
-            int temp = answer[i];
-            answer[i] = answer[answer.length - i - 1];
-            answer[answer.length - i - 1] = temp;
-        }
+       
         return answer;
     }
 }
