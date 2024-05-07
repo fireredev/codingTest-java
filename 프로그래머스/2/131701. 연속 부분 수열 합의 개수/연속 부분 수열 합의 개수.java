25e6293c5e5dead82037bf447e6fd4f0f7e4536f
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-05-07 19:36:39 +0800

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1896.73 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,29 +1,19 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> uniqueSums = new HashSet<>();
-        
-        int n = elements.length;
-        // 원형 수열을 처리하기 위해 길이의 2배를 가진 배열 생성
-        int[] extendedElements = new int[n * 2];
-        for (int i = 0; i < n; i++) {
-            extendedElements[i] = elements[i];
-            extendedElements[i + n] = elements[i];
-        }
-        
-        // 모든 가능한 연속 부분 수열의 합을 계산
-        for (int start = 0; start < n; start++) { // 시작점
-            for (int len = 1; len <= n; len++) { // 부분 수열의 길이
-                int sum = 0;
-                for (int i = start; i < start + len; i++) {
-                    sum += extendedElements[i];
+        Set<Integer> answer = new HashSet<>();
+        for(int i=1;i<=elements.length;i++){
+            for(int j=0;j<elements.length;j++){
+                int sum=0;
+                for (int k = j; k < j + i; k++){
+                    sum+=elements[k%elements.length];
                 }
-                uniqueSums.add(sum);
+                answer.add(sum);
             }
         }
         
-        // 중복을 제거한 합의 개수 반환
-        return uniqueSums.size();
+        
+        
+        return answer.size();
     }
 }
