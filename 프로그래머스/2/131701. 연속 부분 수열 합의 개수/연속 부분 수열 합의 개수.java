2e7255cd14f63cb2698b631712e1787706c0e269
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-05-07 19:50:12 +0800

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 624.15 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,19 +1,29 @@
 import java.util.*;
+
+
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> answer = new HashSet<>();
-        for(int i=1;i<=elements.length;i++){
-            for(int j=0;j<elements.length;j++){
-                int sum=0;
-                for (int k = j; k < j + i; k++){
-                    sum+=elements[k%elements.length];
-                }
-                answer.add(sum);
+        // elements의 2배 크기의 배열 생성
+        int[] newElements = new int[elements.length * 2];
+
+        // newElements의 i와 i + elements.length 위치에 elements[i] 저장
+        for(int i = 0; i < elements.length; i++) {
+            newElements[i] = elements[i];
+            newElements[i + elements.length] = elements[i];
+        }
+
+        // 중복을 제거하기 위한 Set 생성
+        Set<Integer> set = new HashSet<>();
+
+        // 부분 수열의 합 구하기
+        for(int i = 0; i < elements.length; i++) {
+            for(int j = 0; j < elements.length; j++) {
+                // 새 배열에서 j부터 j+i-1까지의 부분 배열의 합을 구하고 Set에 추가
+                set.add(Arrays.stream(newElements, j, j+i).sum());
             }
         }
-        
-        
-        
-        return answer.size();
+
+        // 개수 반환
+        return set.size();
     }
 }
