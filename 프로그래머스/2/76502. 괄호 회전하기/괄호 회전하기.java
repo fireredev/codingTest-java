3f8ec18f87c3c9120482ef20533852b8ab923c27
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-05-09 17:27:10 +0800

    [level 2] Title: 괄호 회전하기, Time: 1.55 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        for(char c: s.toCharArray()){
+            s =s.substring(1)+s.substring(0,1);
+            
+            Stack<Character> t = new Stack<>();
+            boolean checkPair=true;
+            for(char ch : s.toCharArray()){
+                
+                if(ch == '('||ch == '['||ch == '{'){
+                    t.push(ch);
+                }else if(ch == ')'||ch == ']'||ch == '}'){
+                    if(t.isEmpty()){
+                        checkPair=false;
+                        break;
+                    }
+                    
+                    if(pair(t.peek(),ch)){
+                        t.pop();
+                    }else{
+                        checkPair=false;
+                        break;
+                    }
+                }
+            }
+            if(t.isEmpty()&&checkPair) answer++;
+        }
+        return answer;
+    }
+    private boolean pair(char op , char cl){
+        return (op=='(' && cl ==')'||op=='{' && cl =='}'||op=='[' && cl ==']');
+    }
+}
