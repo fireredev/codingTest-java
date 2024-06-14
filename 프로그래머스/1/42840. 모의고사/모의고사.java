422c97edf0a2ae2e81312e4e5216640e5eb4f5cf
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-06-14 16:33:25 +0900

    [level 1] Title: 모의고사, Time: 3.52 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,35 +1,25 @@
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-
-        Map<Integer, Integer> scoreMap = IntStream.range(0, answers.length)
-                .boxed()
-                .collect(Collectors.toMap(
-                        i -> 1,
-                        i -> (answers[i] == one[i % one.length]) ? 1 : 0,
-                        Integer::sum
-                ));
-
-        scoreMap.put(2, IntStream.range(0, answers.length)
-                .map(i -> (answers[i] == two[i % two.length]) ? 1 : 0)
-                .sum());
-
-        scoreMap.put(3, IntStream.range(0, answers.length)
-                .map(i -> (answers[i] == three[i % three.length]) ? 1 : 0)
-                .sum());
-
-        int maxScore = scoreMap.values().stream().max(Integer::compareTo).get();
-
-        return scoreMap.entrySet().stream()
-                .filter(e -> e.getValue() == maxScore)
-                .mapToInt(Map.Entry::getKey)
-                .toArray();
+        
+        int [] one= {1,2,3,4,5};
+        int [] two= {2,1,2,3,2,4,2,5};
+        int [] three ={3,3,1,1,2,2,4,4,5,5};
+        int ocnt=0;
+        int twcnt=0;
+        int thcnt=0;
+        
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==one[i%5]) ocnt++;
+            if(answers[i]==two[i%8]) twcnt++;
+            if(answers[i]==three[i%10]) thcnt++;
+        }
+        List<Integer>li=new ArrayList<>();
+        int score = Math.max(ocnt,Math.max(twcnt,thcnt));
+        if(score == ocnt) li.add(1);
+        if(score == twcnt) li.add(2);
+        if(score == thcnt) li.add(3);
+        
+        return li.stream().mapToInt(i->i).toArray();
     }
 }
