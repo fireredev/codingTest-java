862a8d84e0550dddcc0d47f5e205234147636e53
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-06-17 22:43:56 +0900

    [level 2] Title: 프로세스, Time: 5.10 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] priorities, int location) {
@@ -10,20 +11,13 @@ class Solution {
         for (int i = 0; i < priorities.length; i++) {
             wait.add(i);
         }
-
         // 대기열에서 문서 꺼내기
         while (!wait.isEmpty()) {
             int idx = wait.poll();
-            boolean isHigher = false;
-
-            // 대기열에 남은 문서들 중 더 높은 중요도 있는지 확인
-            for (int i : wait) {
-                if (priorities[i] > priorities[idx]) {
-                    isHigher = true;
-                    break;
-                }
-            }
-
+            boolean isHigher = IntStream.range(0, wait.size())
+                              .mapToObj(i -> wait.toArray()[i])
+                              .mapToInt(i -> priorities[(int)i])
+                              .anyMatch(p -> p > priorities[idx]);
             if (isHigher) {
                 wait.addLast(idx);
             } else {
@@ -34,7 +28,6 @@ class Solution {
                 }
             }
         }
-
         return answer;
     }
 }
