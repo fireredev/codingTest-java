f2e583c18546364874699d171065a0e8271b9945
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-06-17 16:30:23 +0900

    [level 1] Title: 과일 장수, Time: 0.51 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,20 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        List<Integer> aps=new ArrayList<>();
+         int answer = 0;
+
         Arrays.sort(score);
-        
-        for(int i=score.length-1;i>=0;i--){
-            if(score[i]<=k) aps.add(score[i]);
-            else aps.add(k);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
-        
-        for(int i=0;i<aps.size();i++){
-            if(i % m == m-1){
-                answer+=aps.get(i);
-            }
-        }
-        return answer*m;
+
+        return answer;
     }
 }
