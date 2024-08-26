4621a0a300c0a1800e2a0b7a7cf449feb12d6ba6
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-08-26 18:02:52 +0900

    [level 1] Title: 실패율, Time: 10.69 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,34 +1,36 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int N, int[] stages) {
-        HashMap<Integer,Double>m=new HashMap<>();
-        int cnt=1;
-        int fal=0;
-        int trynum=stages.length;
-        while(cnt!=N+1){
-            for(int i =0;i<stages.length;i++){
-                if(cnt==stages[i]){   
-                    fal++;
-                }    
+        int[] stageCounts = new int[N + 2]; // 각 스테이지에서 실패한 플레이어 수
+        int totalPlayers = stages.length;
+
+        // 각 스테이지의 실패자 수 계산
+        for (int stage : stages) {
+            if (stage <= N) {
+                stageCounts[stage]++;
             }
-            if(fal!=0){
-                m.put(cnt,(double)fal / trynum);    
-            }else{
-                m.put(cnt,0.0);
-            }
-            trynum-=fal;
-            cnt++;
-            fal=0;
         }
-        List<Map.Entry<Integer, Double>> sortedList = new ArrayList<>(m.entrySet());
-        sortedList.sort((entry1, entry2) -> {
-            int valueComparison = entry2.getValue().compareTo(entry1.getValue()); 
-            if (valueComparison == 0) {
-                return entry1.getKey().compareTo(entry2.getKey()); 
+
+        // 실패율 계산
+        Map<Integer, Double> failureRates = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            if (totalPlayers > 0) {
+                failureRates.put(i, (double) stageCounts[i] / totalPlayers);
+                totalPlayers -= stageCounts[i]; // 다음 스테이지의 총 플레이어 수 감소
+            } else {
+                failureRates.put(i, 0.0); // 실패자가 없을 경우
             }
-            return valueComparison;
+        }
+
+        // 실패율 기준으로 정렬
+        List<Map.Entry<Integer, Double>> sortedList = new ArrayList<>(failureRates.entrySet());
+        sortedList.sort((entry1, entry2) -> {
+            int valueComparison = entry2.getValue().compareTo(entry1.getValue());
+            return valueComparison != 0 ? valueComparison : entry1.getKey().compareTo(entry2.getKey());
         });
-        
+
+        // 정렬된 스테이지 번호 반환
         return sortedList.stream().mapToInt(Map.Entry::getKey).toArray();
     }
 }
