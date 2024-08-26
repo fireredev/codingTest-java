27f2f7734cf57036461c4e95add71493aba29ba2
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-08-26 15:20:56 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 46.62 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,35 +1,14 @@
 import java.util.*;
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        
-        ArrayList<int[]>li=new ArrayList<>();
-        int extnum = codename(ext);
-        int sortnum = codename(sort_by);
-        int cnt=0;
-        for(int i =0;i<data.length;i++){
-            if(data[i][extnum]<val_ext){
-                li.add(data[i]);
-            }
-        }
-        Collections.sort(li, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return Integer.compare(a[sortnum], b[sortnum]); 
-            }
-        });
-       
-        int[][] answer = new int[li.size()][];
-        for (int i = 0; i < li.size(); i++) {
-            answer[i] = li.get(i); 
-        }
-        
-        
+
+        String[] arr = {"code","date","maximum","remain"};
+        List<String> columnList = Arrays.asList(arr);
+        int extIdx = columnList.indexOf(ext);
+        int sortIdx = columnList.indexOf(sort_by);
+        int[][] answer = Arrays.stream(data).filter(o1 -> o1[extIdx] < val_ext)
+            .sorted((o1 ,o2) -> o1[sortIdx]-o2[sortIdx]).toArray(int[][]::new);
+
         return answer;
     }
-    private int codename(String str){
-        if(str.equals("code")) return 0;
-        else if(str.equals("date")) return 1;
-        else if(str.equals("maximum")) return 2;
-        else return 3;
-    } 
 }
