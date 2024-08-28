07e273901af1046107b560c4db8dfdba34e28f63
박대웅 <148295523+fireredev@users.noreply.github.com>
2024-08-28 18:43:35 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,27 +1,33 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
         Stack<Integer> stack = new Stack<>();
+        int removedDollsCount = 0;
+
         for (int move : moves) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[j][move - 1] != 0) {
-                    if (stack.isEmpty()) {
-                        stack.push(board[j][move - 1]);
-                        board[j][move - 1] = 0;
-                        break;
-                    }
-                    if (board[j][move - 1] == stack.peek()) {
-                        stack.pop();
-                        answer += 2;
-                    } else
-                        stack.push(board[j][move - 1]);
-                    board[j][move - 1] = 0;
-                    break;
+            int doll = getDoll(board, move);
+            if (doll != 0) {
+                if (!stack.isEmpty() && stack.peek() == doll) {
+                    stack.pop(); // 같은 인형이면 제거
+                    removedDollsCount += 2; // 제거된 두 인형 카운트
+                } else {
+                    stack.push(doll); // 다른 인형이면 추가
                 }
             }
         }
-        return answer;
+
+        return removedDollsCount; // 제거된 인형의 개수 반환
+    }
+
+    private int getDoll(int[][] board, int move) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][move - 1] != 0) {
+                int doll = board[row][move - 1];
+                board[row][move - 1] = 0; // 해당 위치를 비움
+                return doll; // 뽑은 인형 반환
+            }
+        }
+        return 0; // 인형이 없으면 0 반환
     }
 }
